854cfaaebe44c6919c2f20f13991d5bf6b373d19
jenifav <jennifer.h.favaloro>
2025-05-18 15:08:06 -0400

    re-commit with R data visualization tutorial

--- Data Viz with RStudio.R
+++ Data Viz with RStudio.R
@@ -18,10 +18,7 @@ install.packages('pacman')
 
 pacman::p_load('tidyverse',	'ggplot2',	'dplyr',	'RColorBrewer',	
                'reshape2',	'corrplot',	'patchwork', 'devtools',
-               'plotly', 'vcd')
-
-devtools::install_github("haleyjeppson/ggmosaic", upgrade = "never") 
-library(ggmosaic)
+               'plotly', 'vcd', 'ggmosaic')
 
 # -----------------------------------------------------------------------------
 # 1. Base R Plotting System
--- install.R
+++ install.R
@@ -5,7 +5,7 @@ cat("R version:", R.version$version.string, "\n")
 # Install CRAN packages with more robust error handling
 pkg_list <- c("tidyverse", "ggplot2", "dplyr", "RColorBrewer", 
               "reshape2", "corrplot", "patchwork", "devtools",
-              "plotly", "vcd")
+              "plotly", "vcd", "ggmosaic")
 
 for (pkg in pkg_list) {
   cat("Attempting to install", pkg, "...\n")
@@ -26,23 +26,23 @@ for (pkg in pkg_list) {
 }
 
 # Install GitHub packages
-cat("Attempting to install ggmosaic from GitHub...\n")
-tryCatch({
-  if (!require("ggmosaic", quietly = TRUE)) {
-    if (!require("remotes", quietly = TRUE)) {
-      install.packages("remotes", repos = "https://cloud.r-project.org")
-    }
-    remotes::install_github("haleyjeppson/ggmosaic", upgrade = "never")
-    if (require("ggmosaic", quietly = TRUE)) {
-      cat("ggmosaic successfully installed\n")
-    } else {
-      cat("WARNING: ggmosaic installation was attempted but package cannot be loaded\n")
-    }
-  } else {
-    cat("ggmosaic is already installed\n")
-  }
-}, error = function(e) {
-  cat("ERROR installing ggmosaic:", conditionMessage(e), "\n")
-})
-
-cat("Package installation complete\n")
+# cat("Attempting to install ggmosaic from GitHub...\n")
+# tryCatch({
+#   if (!require("ggmosaic", quietly = TRUE)) {
+#     if (!require("remotes", quietly = TRUE)) {
+#       install.packages("remotes", repos = "https://cloud.r-project.org")
+#     }
+#     remotes::install_github("haleyjeppson/ggmosaic", upgrade = "never")
+#     if (require("ggmosaic", quietly = TRUE)) {
+#       cat("ggmosaic successfully installed\n")
+#     } else {
+#       cat("WARNING: ggmosaic installation was attempted but package cannot be loaded\n")
+#     }
+#   } else {
+#     cat("ggmosaic is already installed\n")
+#   }
+# }, error = function(e) {
+#   cat("ERROR installing ggmosaic:", conditionMessage(e), "\n")
+# })
+# 
+# cat("Package installation complete\n")
